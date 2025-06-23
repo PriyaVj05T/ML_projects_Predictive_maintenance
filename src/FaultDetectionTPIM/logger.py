2be9768e53382983330435133e3e487acec3e4bc
PriyaVj05T <vijayvargiyapriya05@gmail.com>
2025-06-23 16:11:20 +0530

    logger session issue resolved, testing done

--- src/FaultDetectionTPIM/logger.py
+++ src/FaultDetectionTPIM/logger.py
@@ -1,6 +1,11 @@
+
+
 import os
 import logging
 from datetime import datetime
+from importlib import reload
+
+reload(logging)
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_path=os.path.join(os.getcwd(),"logs")
@@ -12,3 +17,10 @@ logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILEPATH,
                     format="[%(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s"
                     )
+
+# Optional: also log to console
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter("[%(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s")
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
