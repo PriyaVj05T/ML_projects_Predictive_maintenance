837b40a1e2c4796b030ac7607a6e0e3d8b8d0277
PriyaVj05T <vijayvargiyapriya05@gmail.com>
2025-06-25 17:03:09 +0530

    data ingestion modified

--- src/FaultDetectionTPIM/components/data_ingestion.py
+++ src/FaultDetectionTPIM/components/data_ingestion.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 from src.FaultDetectionTPIM.logger import logging
+import os
 
 class DataIngestion:
     def __init__(self):
         pass
+
     def initiate_data_ingestion(self):
-        pass
+        logging.info("Data Ingestion Started")
+
+        try:
+            #reading data
+            pd.read_csv(Path(os.path.join("notebooks\data\predictive_maintenance.csv")))
+            logging.info("i have read dataset as df")
+
+            #splitting data
+
+            logging.info("Here i have performed train test split")
+
+        except Exception as e:    
+            pass
      
