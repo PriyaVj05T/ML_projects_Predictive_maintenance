26c26efb5dd9e73a19f530238feeb2cf5c009cd9
PriyaVj05T <vijayvargiyapriya05@gmail.com>
2025-07-02 14:19:10 +0530

    logs, path added

--- src/FaultDetectionTPIM/components/data_ingestion.py
+++ src/FaultDetectionTPIM/components/data_ingestion.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from src.FaultDetectionTPIM.logger import logging
+from src.FaultDetectionTPIM.exception import CustomException
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+from pathlib import Path
 import os
 
 class DataIngestion:
@@ -13,12 +17,13 @@ class DataIngestion:
 
         try:
             #reading data
-            pd.read_csv(Path(os.path.join("notebooks\data\predictive_maintenance.csv")))
+            data=pd.read_csv(Path(os.path.join("notebooks/data", "predictive_maintenance.csv")))
             logging.info("i have read dataset as df")
 
             #splitting data
-
             logging.info("Here i have performed train test split")
+            train_data,test_data=train_test_split(data, test_size=0.25)
+            logging.info("Here train test split completed")
 
         except Exception as e:    
             pass
