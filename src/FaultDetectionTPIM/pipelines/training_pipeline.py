1377e3fa9dc0d053b0667b13d52bbf55a2af2652
PriyaVj05T <vijayvargiyapriya05@gmail.com>
2025-07-03 16:10:52 +0530

    pipeline modified

---
+++ src/FaultDetectionTPIM/pipelines/training_pipeline.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import pandas as pd
+from src.FaultDetectionTPIM.components.data_ingestion import DataIngestion
+from src.FaultDetectionTPIM.logger import logging
+from src.FaultDetectionTPIM.exception import CustomException
